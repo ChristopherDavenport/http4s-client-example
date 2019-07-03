3f6294445c91189f08a617409432e755d370ef5b
Scala Steward <me@scala-steward.org>
2019-07-03 05:36:14 +0200

    Update http4s-blaze-client, ... to 0.20.3

--- src/main/scala/io/chrisdavenport/clientexample/ClientExampleServer.scala
+++ src/main/scala/io/chrisdavenport/clientexample/ClientExampleServer.scala
@@ -1,14 +1,14 @@
 package io.chrisdavenport.clientexample
 
 import cats.implicits._
-import org.http4s.server.blaze.BlazeBuilder
-import org.http4s.client.blaze.Http1Client
+import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.dsl._
 import org.http4s._
 import org.http4s.client._
 import cats.effect._
 import fs2._
 import scala.concurrent.ExecutionContext
+import org.http4s.server.blaze.BlazeServerBuilder
 
 object ClientExampleServer {
     def serve[F[_]](implicit Effect: Effect[F], EC: ExecutionContext) : Stream[F, StreamApp.ExitCode] = 
@@ -17,16 +17,16 @@ object ClientExampleServer {
             client <- Http1Client.stream[F]()
 
             service = getGoogleService[F](client)
-            exitCode <- BlazeBuilder[F].bindHttp(8080, "0.0.0.0").mountService(service).serve
+            exitCode <- BlazeServerBuilder[F].bindHttp(8080, "0.0.0.0").mountService(service).serve
         } yield exitCode
 
 
-    def getGoogleService[F[_]: Effect](c: Client[F]): HttpService[F] = {
+    def getGoogleService[F[_]: Effect](c: Client[F]): HttpRoutes[F] = {
         val _ = c
         val dsl = new Http4sDsl[F]{}
         import dsl._
 
-        HttpService[F]{
+        HttpRoutes.of[F]{
             case GET -> Root / "google" => 
                 Ok()
         }
