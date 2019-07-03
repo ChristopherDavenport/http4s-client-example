6507c0c90b5509553a5385ce419c6e2abcf446b9
Chris Davenport <chris@christopherdavenport.tech>
2019-07-03 09:01:48 -0700

    Update Example

--- src/main/scala/io/chrisdavenport/clientexample/ClientExampleServer.scala
+++ src/main/scala/io/chrisdavenport/clientexample/ClientExampleServer.scala
@@ -1,32 +1,34 @@
 package io.chrisdavenport.clientexample
 
-import cats.implicits._
-import org.http4s.server.blaze.BlazeBuilder
-import org.http4s.client.blaze.Http1Client
+import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.dsl._
 import org.http4s._
+import org.http4s.implicits._
 import org.http4s.client._
 import cats.effect._
 import fs2._
-import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.global
 
 object ClientExampleServer {
-    def serve[F[_]](implicit Effect: Effect[F], EC: ExecutionContext) : Stream[F, StreamApp.ExitCode] = 
+    def serve[F[_]](implicit Effect: ConcurrentEffect[F], T: Timer[F]) : Stream[F, ExitCode] = 
         for {
             _ <- Stream.eval(Sync[F].delay(println("Starting Client")))
-            client <- Http1Client.stream[F]()
+            client <- Stream.resource(BlazeClientBuilder[F](global).resource)
 
             service = getGoogleService[F](client)
-            exitCode <- BlazeBuilder[F].bindHttp(8080, "0.0.0.0").mountService(service).serve
+            exitCode <- BlazeServerBuilder[F].bindHttp(8080, "0.0.0.0")
+                .withHttpApp(service.orNotFound)
+                .serve
         } yield exitCode
 
 
-    def getGoogleService[F[_]: Effect](c: Client[F]): HttpService[F] = {
+    def getGoogleService[F[_]: Effect](c: Client[F]): HttpRoutes[F] = {
         val _ = c
         val dsl = new Http4sDsl[F]{}
         import dsl._
 
-        HttpService[F]{
+        HttpRoutes.of[F]{
             case GET -> Root / "google" => 
                 Ok()
         }
